b108931dd18288be237064935900bff8bef764a7
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-07-20 11:50:20 +0800

    test

--- src/types/cart.tsx
+++ src/types/cart.tsx
@@ -33,19 +33,3 @@ export interface OrderList {
   payed: boolean,
   customer: User,
 }
-
-
-// export enum CartActionTypes {
-//   ADD_ITEM_TO_CART = 'ADD_ITEM_TO_CART',
-//   DELETE_ITEM_TO_CART = 'DELETE_ITEM_TO_CART',
-// }
-
-// interface CartAddItem {
-//   type: CartActionTypes.ADD_ITEM_TO_CART;
-//   payload: any[]
-// }
-// interface CartDeleteItem {
-//   type: CartActionTypes.DELETE_ITEM_TO_CART ;
-//   payload: string;
-// }
-// export type CartActions = CartAddItem | CartDeleteItem;
